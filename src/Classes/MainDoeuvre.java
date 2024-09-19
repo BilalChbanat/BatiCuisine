312f9b3e6f89cfa821842b5553c85fd59080daf8
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-09-19 11:19:09 +0100

    creating the MainDoeuvre Entity

---
+++ src/Classes/MainDoeuvre.java
@@ -0,0 +1,50 @@
+package Classes;
+
+public class MainDoeuvre {
+    private int id ;
+    private double tauxHoraire ;
+    private double heuresTravail ;
+    private double productiviteOuvrier ;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getTauxHoraire() {
+        return tauxHoraire;
+    }
+
+    public void setTauxHoraire(double tauxHoraire) {
+        this.tauxHoraire = tauxHoraire;
+    }
+
+    public double getHeuresTravail() {
+        return heuresTravail;
+    }
+
+    public void setHeuresTravail(double heuresTravail) {
+        this.heuresTravail = heuresTravail;
+    }
+
+    public double getProductiviteOuvrier() {
+        return productiviteOuvrier;
+    }
+
+    public void setProductiviteOuvrier(double productiviteOuvrier) {
+        this.productiviteOuvrier = productiviteOuvrier;
+    }
+
+    @Override
+    public String toString() {
+        return "\nMainDoeuvre{" +
+                "id=" + id +
+                ", tauxHoraire=" + tauxHoraire +
+                ", heuresTravail=" + heuresTravail +
+                ", productiviteOuvrier=" + productiviteOuvrier +
+                '}';
+    }
+}
