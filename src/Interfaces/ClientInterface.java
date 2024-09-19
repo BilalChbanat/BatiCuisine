2c69d1950364c2fd558bb079cb76184374180d3c
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-09-19 11:38:45 +0100

    creating the  Cilent interface and adding there function

---
+++ src/Interfaces/ClientInterface.java
@@ -0,0 +1,12 @@
+package Interfaces;
+
+import Models.Client;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ClientInterface {
+    void addClient(Client client);
+    Optional<Client> getClientByName(String name);
+    List<Client> getAllClients();
+}
