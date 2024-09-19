245f264a5a57c5255a306171a114db07b3e1ef51
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-09-19 11:55:15 +0100

    creating the  DevisRepository and implemanting the interface functions

---
+++ src/Repositories/DevisRepository.java
@@ -0,0 +1,10 @@
+package Repositories;
+
+import Interfaces.DevisInterface;
+
+public class DevisRepository implements DevisInterface {
+    @Override
+    public double calculateTax(double baseAmount, double taxRate) {
+        return 0;
+    }
+}
