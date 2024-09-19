c5b870c10bb82e25418fddb9bcf617a7edc41c64
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-09-19 11:19:36 +0100

    creating the Project Entity

--- src/Classes/Project.java
+++ src/Classes/Project.java
@@ -1,4 +1,62 @@
 package Classes;
 
+import Enums.ProjectStatus;
+
 public class Project {
+
+    private int id;
+    private String name;
+    private double profitMargin;
+    private double totalCost;
+    private ProjectStatus projectStatus;
+
+    public Project(int id, String name, double profitMargin, double totalCost, ProjectStatus projectStatus) {
+        this.id = id;
+        this.name = name;
+        this.profitMargin = profitMargin;
+        this.totalCost = totalCost;
+        this.projectStatus = projectStatus;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getProfitMargin() {
+        return profitMargin;
+    }
+
+    public void setProfitMargin(double profitMargin) {
+        this.profitMargin = profitMargin;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public ProjectStatus getProjectStatus() {
+        return projectStatus;
+    }
+
+    public void setProjectStatus(ProjectStatus projectStatus) {
+        this.projectStatus = projectStatus;
+    }
 }
+
+
